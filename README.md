c7221c2e4ea510f37e5a2232dfdf4debd5e390dc
Louis A <louis.auneau.c@gmail.com>
2016-02-23 23:43:55 +0100

    Ajout de la vue d'une tâche (récupération avec ORM + vue)

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-studeal
-=======
+Symfony Todo
+============
 
+Projet de todolist réalisé avec le framework PHP Symfony, dans sa version 2.8. Ce projet a pour simple but de prendre en main le framework et ses outils.
+
+Todolist project developed with Symfony PHP Framework in its 2.8 version. This project has for only aim to handle symfony and all of its tools.
+
+---
 A Symfony project created on February 20, 2016, 12:21 pm.
--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -6,7 +6,7 @@ use AppBundle\Entity\Task;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use UserBundle\Entity\User;
+use Symfony\Component\HttpFoundation\Response;
 
 class TodoController extends Controller
 {
@@ -35,18 +35,44 @@ class TodoController extends Controller
      */
     public function taskListAction(Request $request){
         // If user is not logged, we redirect him to the homepage.
-        if(!$this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')){
+        if(!$this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
             return $this->redirect($this->generateUrl("todo_home"));
-        }
 
         // Retrieving Entity Manager
         $em = $this->getDoctrine()->getManager();
+
+        // Creating a task
         $task = new Task();
         $task->setTitle("Tâche 1");
         $task->setEndDate(new \DateTime("now"));
         $task->setDescription("Il s'agit de la toute première tâche !");
 
+        /*$em->persist($task);
+        $em->flush();*/
+
         return $this->render('AppBundle::task_list.html.twig');
     }
 
+    /**
+     * @param Request $request Request object automatically passed when action is called.
+     * @param $id Id of the task that is displayed. If it doesn't exist, the page is redirected.
+     *
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     */
+    public function viewTaskAction(Request $request, $id){
+        // If user is not logged, we redirect him to the homepage.
+        if(!$this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED'))
+            return $this->redirect($this->generateUrl("todo_home"));
+
+        // Retrieving Entity Manager and current task
+        $em = $this->getDoctrine()->getManager();
+        $currentTask = $em->getRepository("AppBundle:Task")->find($id);
+
+        // Displaying task and managing errors
+        if(is_null($currentTask))
+            return $this->redirect($this->generateUrl("todo_task_list"));
+
+        $viewParams = ["task" => $currentTask];
+        return $this->render("AppBundle::task.html.twig", $viewParams);
+    }
 }
